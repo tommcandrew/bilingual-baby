fe286f21eb5ef9729bdda8d4907a1a5584ab2723
Thomas McAndrew <tommcandrew@hotmail.com>
2020-09-14 12:53:38 +0100

    install dependencies, remove boiletplate and add first test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+configure({ adapter: new Adapter() });
+
+describe("App", () => {
+  it("should render hello world heading", () => {
+    const component = shallow(<App />);
+    expect(component.text()).toContain("Hello World");
+  });
 });
